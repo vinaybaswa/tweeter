ebf247d1174a981b040fd2988e7adb30759b5a61
Vinay Baswa <vinaybaswa@gmail.com>
2020-09-10 13:23:20 +0000

    display validation errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   console.log("DOM ready from char-counter")
 
+  const renderError = function(message) {
+    $('#error').remove();
+    //create the new element 
+    let error = $(`<div id="error"> ${message} </div>`);
+    //append to new tweet 
+    $('.new-tweet').prepend(error);
+    $('#error').slideDown("slow"); //animations;
+  }
+
   $('#tweet-text').keyup(function() {
     const tweet = $(this).val();
     console.log(tweet);
@@ -10,18 +19,23 @@ $(document).ready(function() {
 
     if (tweet.length > 140) {
       limit.css('color', 'red');
-      alert("tweet too long, please respect 140 characters rule!!")
+      renderError("Message exceeded character length of 140 characters.");
+      //console.log($('#submit-btn')[0]);
+      $('#submit-btn').prop('disabled', true); //disable it
+    } else {
+      limit.css('color', 'black');
+      $('#error').remove();
+      $('#submit-btn').prop('disabled', false); //enable it so it can submit
     }
   });
 
   $('#submit-btn').click(function() {
     $('.counter').val(140);
     if ($('#tweet-text').val() === "") {
-      alert("Blanks tweets are no good!!");
+      renderError("Blanks tweets are no good!!");
 
     } else if ($('#tweet-text').val().length > 140) {
-      ralert("tweet too long, please respect 140 characters rule!!")
-
+      renderError("tweet too long, please respect 140 characters rule!!")
     }
   })
 
