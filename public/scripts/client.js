9a193d2633a2b276429ff6d0e11d151fa2d65d1b
Vinay Baswa <vinaybaswa@gmail.com>
2020-09-10 01:00:35 +0000

    implemented jquery get method to get fetch tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
 const data = [{
     "user": {
       "name": "Newton",
@@ -32,7 +23,7 @@ const data = [{
 ]
 
 const createTweetElement = function(tweet) {
-    const $tweet = $(`<div class = "tweet">
+  const $tweet = $(`<div class = "tweet">
   <div class="tweet-header">
     <img src="${tweet.user.avatars}" class="tweeter-icon"> 
     <p class="tweeter-name"> ${tweet.user.name} </p>
@@ -44,35 +35,21 @@ const createTweetElement = function(tweet) {
     <p class="small-icons"> <i class="fa fa-flag"></i> <i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p>
   </div>
   </div>`);
-    return $tweet;
-    //$(".tweets-container").append($item);
-  }
-  //const $tweet = createTweetElement(data);
+  return $tweet;
+}
 
+//Creates element for each tweet
 const renderTweets = function(tweets) {
-  // loops through tweets
   for (const tweet of tweets) {
-    // calls createTweetElement for each tweet
     const $tweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
     $(document).ready(function() {
-      //console.log("we are ready")
-      //console.log(createTweetElement(data))
-      $('.tweets-container').append($tweet);
+      $(".tweets-container").append($tweet);
     });
   }
 }
 
-// const createTweetElement = function(tweet) {
-//   let $tweet = /* Your code for creating the tweet element */
-//     // ...
-//     return $tweet;
-// }
-
 renderTweets(data);
 
-
-
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-//$('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+$.get("/tweets")
+  .then(responnse => renderTweets(responnse))
+  .catch(e => $(".tweets-container").append(`<h1>SOMETHING'S WRONG</h1>`))
